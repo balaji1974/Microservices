d622523452347a791bc9e7fe519f3a3c8e157813
Balaji Thiagarajan <thiagarajan.balaji@gmail.com>
2021-04-19 00:36:00 +0300

    Retry & Fallback config

--- login-service/src/main/java/com/bala/microservices/loginservice/controller/LoginConroller.java
+++ login-service/src/main/java/com/bala/microservices/loginservice/controller/LoginConroller.java
@@ -1,14 +1,35 @@
 package com.bala.microservices.loginservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
 public class LoginConroller {
+	
+	Logger logger=LoggerFactory.getLogger(LoginConroller.class);
 	
 	@GetMapping("/login")
 	public String doLogin() {
 		return "Successful";
 	}
+	
+	
+	@GetMapping("/hello")
+	@Retry(name="hello-api" , fallbackMethod="helloFallback")
+	public String helloWorld() throws RuntimeException{
+		logger.info("Method is being called");
+		if(true) throw new RuntimeException();
+		return "Hello World";
+	}
+	
+	public String helloFallback(Exception e) {
+		return "This is fallback response";
+	}
 
 }
+
+
